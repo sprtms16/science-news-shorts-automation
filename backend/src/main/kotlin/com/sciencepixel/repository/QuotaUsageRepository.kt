d18c8d1c550c84e1ccc9e7f7b7fae50f65eccfd4
sprtms16 <sprtms16@gmail.com>
2026-02-04 17:58:33 +0900

    chore: remove zombie code in backend/src (legacy leftovers)

--- backend/src/main/kotlin/com/sciencepixel/repository/QuotaUsageRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.sciencepixel.repository
-
-import com.sciencepixel.domain.QuotaUsage
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface QuotaUsageRepository : MongoRepository<QuotaUsage, String>
